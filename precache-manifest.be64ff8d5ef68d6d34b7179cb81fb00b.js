1831db1c33fa169f137c83fe566f16be4f55b4c4
PilarFernandez1304 <ccentepilar@gmail.com>
2019-09-10 01:01:47 -0500

    Updates

---
+++ precache-manifest.be64ff8d5ef68d6d34b7179cb81fb00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0f324f6908eebe8872b4dac321d2ae",
+    "url": "/Reto-Blazar/index.html"
+  },
+  {
+    "revision": "ec7ea831cd479f9b10bf",
+    "url": "/Reto-Blazar/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6323a2f475d731dafee7",
+    "url": "/Reto-Blazar/static/js/2.1aac4db5.chunk.js"
+  },
+  {
+    "revision": "ec7ea831cd479f9b10bf",
+    "url": "/Reto-Blazar/static/js/main.3422b059.chunk.js"
+  },
+  {
+    "revision": "d660a45f18c473ed1d73",
+    "url": "/Reto-Blazar/static/js/runtime~main.0a6d7412.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reto-Blazar/static/media/logo.5d5d9eef.svg"
+  }
+]);
